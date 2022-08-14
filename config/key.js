6caa65883293f808170f6865f784fbc386e2ebf8
Michael Chou <michael861227@gmail.com>
2022-08-15 00:04:02 +0800

    Add key.js

---
+++ config/key.js
@@ -0,0 +1,17 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+      clientID:
+        "Your ID",
+      clientSecret: "Your secret",
+    },
+    mongodb: {
+      dbURI:
+        "Your dbURI",
+    },
+    session: {
+      cookieKey: "Your key",
+    },
+  };
+  
